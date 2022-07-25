2e441363ba18cf88d33f4efd48a465b3c4fa9dae
princejohnnie <princejohnc35@gmail.com>
2022-07-25 17:28:36 +0100

    Added settings.xml, pref_resources.xml and implemented Settings screen UI

--- app/src/main/java/com/yungjohn/mynotes/ui/SettingsFragment.kt
+++ app/src/main/java/com/yungjohn/mynotes/ui/SettingsFragment.kt
@@ -5,27 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 import com.yungjohn.mynotes.R
 
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+
+        setPreferencesFromResource(R.xml.settings, rootKey)
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
-    }
 
 }
