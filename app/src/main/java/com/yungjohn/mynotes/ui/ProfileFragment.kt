c1954fac84ffc74541b12e5d51d9df8b36f246e3
princejohnnie <princejohnc35@gmail.com>
2022-07-25 21:56:32 +0100

    Added ProfileFragment.kt, ProfileViewModel.kt and fragment_profile.xml

---
+++ app/src/main/java/com/yungjohn/mynotes/ui/ProfileFragment.kt
@@ -0,0 +1,36 @@
+package com.yungjohn.mynotes.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yungjohn.mynotes.R
+import com.yungjohn.mynotes.databinding.FragmentProfileBinding
+import com.yungjohn.mynotes.viewmodel.ProfileViewModel
+
+class ProfileFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ProfileFragment()
+    }
+
+    private lateinit var viewModel: ProfileViewModel
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
